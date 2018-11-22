a493e532a111bcf0da31cf15e4eb5a21193f05a4
Ad Ruigrok van der Werve <adruigrokvanderwerve@wcw-stud-145-109-49-252.wireless.uva.nl>
2018-11-22 14:22:44 +0100

    fixed link and axes

--- Homework/Week_3/week_3.js
+++ Homework/Week_3/week_3.js
@@ -5,82 +5,77 @@ var fileName = "data.json";
 var txtFile = new XMLHttpRequest();
 txtFile.onreadystatechange = function() {
 
-if (txtFile.readyState === 4 && txtFile.status == 200) {
-data = JSON.parse(txtFile.responseText);
+  if (txtFile.readyState === 4 && txtFile.status == 200) {
+    data = JSON.parse(txtFile.responseText);
 
-var range = [];
+    var range = [];
 
-keys = Object.keys(data);
-// obtain max and min values of date, represented as YYYYMMDD
-var keys_length = keys.length;
-var domain_max_ = Math.max.apply(null, keys);
-var domain_max_ = domain_max_.toString();
-var domain_min_ = Math.min.apply(null, keys);
-var domain_min_ = domain_min_.toString();
+    keys = Object.keys(data);
+    // obtain max and min values of date, represented as YYYYMMDD
+    var keys_length = keys.length;
+    var domain_max_ = Math.max.apply(null, keys);
+    var domain_max_ = domain_max_.toString();
+    var domain_min_ = Math.min.apply(null, keys);
+    var domain_min_ = domain_min_.toString();
 
+    range.push(domain_min_, domain_max_);
 
-range.push(domain_min_, domain_max_);
+    values = Object.values(data);
 
-values = Object.values(data);
+    // strip YYYYMMDD to another date notation
+    var dateString = domain_max_;
+    var year = dateString.substring(0,4);
+    var month = dateString.substring(4,6);
+    var day = dateString.substring(6,8);
 
-// strip YYYYMMDD to another date notation
-var dateString = domain_max_;
-var year = dateString.substring(0,4);
-var month = dateString.substring(4,6);
-var day = dateString.substring(6,8);
+    var date_max = new Date(year, month-1, day);
 
-var date_max = new Date(year, month-1, day);
+    var dateString1 = domain_min_;
+    var year1 = dateString1.substring(0,4);
+    var month1 = dateString1.substring(4,6);
+    var day1 = dateString1.substring(6,8);
 
-var dateString1 = domain_min_;
-var year1 = dateString1.substring(0,4);
-var month1 = dateString1.substring(4,6);
-var day1 = dateString1.substring(6,8);
+    var date_min = new Date(year1, month1-1, day1);
 
-var date_min = new Date(year1, month1-1, day1);
+    var list = [];
 
-var list = [];
+    var domain = [];
+    // get value of TX that belongs to key(YYYYMMDD)
+    keys.forEach(function(key)  {
+      value = (data[key]["TX"]);
+      list.push(value);
+    });
 
-var domain = [];
-// get value of TX that belongs to key(YYYYMMDD)
-keys.forEach(function(key)  {
-  value = (data[key]["TX"]);
-  list.push(value);
-});
+    // obtain max and min values of TX
 
-// obtain max and min values of TX
+    range_max_ = Math.max.apply(null, list);
+    range_min_ = Math.min.apply(null, list);
 
-range_max_ = Math.max.apply(null, list);
-range_min_ = Math.min.apply(null, list);
+    domain.push(range_min_, range_max_);
 
-// range of plot, which is inserted into createTransform
-var x_values = createTransform([domain_min_, domain_max_],[0, 100])
-var y_values = createTransform([range_min_, range_max_],[0,600])
-domain.push(range_min_, range_max_);
+    // scale both x and y directions of plot
+    transformY = createTransform(domain, [700, 0]);
+    transformX = createTransform(range, [0, 500]);
+    list.forEach(function(element){
+      transformY(element);
+    })
 
-// scale both x and y directions of plot
-transformY = createTransform(domain, [0, 700]);
-transformX = createTransform(range, [0, 500]);
-list.forEach(function(element){
-  console.log(transformY(element));
-})
-console.log(list);
+    let lines = document.querySelector("canvas").getContext("2d");
 
-let lines = document.querySelector("canvas").getContext("2d");
+    lines.moveTo(100, transformY(list[0]));
 
-lines.moveTo(100, transformY(list[0]));
+    // draw keys_length(18) connected points
+    for (i = 1; i < keys_length; i++){
+      lines.lineTo(100 + i * 45, transformY(list[i]))
+      lines.moveTo(100 + i * 45, transformY(list[i]))
+    }
 
-// draw keys_length(18) connected points
-for (i = 1; i < keys_length; i++){
-  lines.lineTo(100 + i * 45, transformY(list[i]))
-  lines.moveTo(100 + i * 45, transformY(list[i]))
+    lines.stroke()
+  }
 }
 
-lines.stroke()
-    }
-  }
-
-  txtFile.open("GET", fileName);
-  txtFile.send();
+txtFile.open("GET", fileName);
+txtFile.send();
 
 function createTransform(domain, range){
     var domain_min = domain[0];
@@ -115,17 +110,39 @@ for (let y = 100; y < 910; y += 100) {
 
 // loop to place values of x,y values at the axes
 
-var x_coordinaat = 60;
-var y_coordinaat = 20;
-for (var i = 0; i < 10; i++)
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  ctx.font = "15px Arial";
-  ctx.strokeText("51",x_coordinaat,y_coordinaat);
-  x_coordinaat += 100
-  y_coordinaat += 100
+      var x_coordinaat = 60;
+      var y_coordinaat = 14;
+      var y = 189
+      var canvas = document.getElementById("canvas");
+      var ctx = canvas.getContext("2d");
+      for (var i = 0; i < 8; i++){
+          ctx.font = "15px Arial";
+          ctx.strokeText(y,x_coordinaat,y_coordinaat);
+          y_coordinaat += 101
+          y -= 20
+        };
 
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  ctx.font = "15px Arial";
-  ctx.strokeText("51",x_coordinaat,y_coordinaat);
+      var x_coordinaat = 100;
+      var y_coordinaat = 830;
+      var x = 1
+      var canvas = document.getElementById("canvas");
+      var ctx = canvas.getContext("2d");
+      // months =
+      for (var i = 0; i < 18; i++){
+          ctx.font = "15px Arial";
+          ctx.strokeText(x,x_coordinaat,y_coordinaat);
+          x_coordinaat += 45
+          x += 1
+        };
+
+
+        ctx.font = '20px arial';
+        ctx.fillText('Datum (november)', 435, 865);
+
+        ctx.font = '20px arial';
+        ctx.fillText('Max. Temp. /0.1 graad', 865, 475);
+
+        ctx.font = '40px arial';
+        ctx.fillText('Ad Ruigrok van der Werve', 100, 950);
+        ctx.fillText('Javascript week3', 100, 1050);
+        ctx.fillText('', 100, 1150);
